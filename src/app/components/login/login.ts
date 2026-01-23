324c42a7fa2240932400f27689fb5f28cb8a61d8
MasterMarv <mbig6820@gmail.com>
2026-01-23 08:30:35 +0100

    login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginModel } from '../../model/login.model';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
 
+export class Login {
+  login: LoginModel = { username: '', password: '' };
+
+  performLogin() {
+    this.login = {
+      username: this.login.username,
+      password: this.login.password,
+    };
+    console.log('Logging in with:', this.login);
+  }
 }
