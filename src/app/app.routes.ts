501560526637b7be89d9f0e77f782ead9ec062a1
MasterMarv <mbig6820@gmail.com>
2026-01-29 10:41:31 +0100

    start page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { CreateMeal } from './components/create-meal/create-meal';
 import { CreateUser } from './components/create-user/create-user';
 import { Login } from './components/login/login';
+import { StartPage } from './components/start-page/start-page';
 
 export const routes: Routes = [
+  { path: '', component: StartPage },
   { path: 'create-meal', component: CreateMeal },
   { path: 'create-user', component: CreateUser },
   { path: 'login', component: Login },
---
+++ src/app/components/start-page/start-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-start-page',
+  imports: [RouterLink],
+  templateUrl: './start-page.html',
+  styleUrl: './start-page.scss',
+})
+export class StartPage {
+
+}
