411b65df8ba87a292ceee79df85bedece3d23e35
W_lfle, Noel <noel.woelfle@sybit.de>
2026-02-06 11:26:34 +0100

    start-page component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { CreateMeal } from './components/create-meal/create-meal';
 import { CreateUser } from './components/create-user/create-user';
 import { Login } from './components/login/login';
 import { Swipe } from './components/swipe/swipe';
+import { StartPage } from './components/start-page/start-page';
 
 export const routes: Routes = [
+  { path: '', component: StartPage },
   { path: 'create-meal', component: CreateMeal },
   { path: 'create-user', component: CreateUser },
   { path: 'login', component: Login },
